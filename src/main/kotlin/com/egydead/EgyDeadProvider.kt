760ad559d5f10e7c5c035bc226ba8437c8021ff4
djemilinedjmou-cell <djemilinedjmou@gmail.com>
2025-12-28 16:46:30 +0100

    Delete src/main/kotlin/com/egydead directory
    Signed-off-by: djemilinedjmou-cell <djemilinedjmou@gmail.com>

--- src/main/kotlin/com/egydead/EgyDeadProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.egydead
-
-import com.lagradost.cloudstream3.*
-
-class EgyDeadProvider : MainAPI() {
-    override var name = "EgyDead"
-    override var mainUrl = "https://egydead.fyi"
-}
