abe066a30ada8b9a40b5163711f9d52b12ee4710
djemilinedjmou-cell <djemilinedjmou@gmail.com>
2025-12-28 16:36:21 +0100

    Update EgyDeadProvider.kt
    Signed-off-by: djemilinedjmou-cell <djemilinedjmou@gmail.com>

--- src/main/kotlin/com/egydead/EgyDeadProvider.kt
+++ src/main/kotlin/com/egydead/EgyDeadProvider.kt
@@ -16,6 +16,33 @@ class EgyDeadProvider : MainAPI() {
     )
 
     override val mainPage = mainPageOf(
-        "$mainUrl/home" to "أحدث الإضافات"
+        "$mainUrl/home" to "أحدث الأفلام"
     )
+
+    override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+
+        val document = app.get(request.data).document
+
+        val items = document.select(".movie-item").mapNotNull { element ->
+            val link = element.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val title = element.selectFirst("a")?.attr("title") ?: "بدون عنوان"
+            val poster = element.selectFirst("img")?.attr("src")
+
+            newSearchResponse(
+                title,
+                fixUrl(link),
+                TvType.Movie
+            ) {
+                posterUrl = fixUrl(poster)
+            }
+        }
+
+        return newHomePageResponse(
+            listOf(HomePageList(request.name, items)),
+            false
+        )
+    }
 }
