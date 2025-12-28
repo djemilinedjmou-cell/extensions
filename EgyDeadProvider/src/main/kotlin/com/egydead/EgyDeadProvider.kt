a08405b18565046a11dc2e4a5e12492df297ccb6
djemilinedjmou-cell <djemilinedjmou@gmail.com>
2025-12-28 16:48:38 +0100

    Create EgyDeadProvider.kt
    Signed-off-by: djemilinedjmou-cell <djemilinedjmou@gmail.com>

---
+++ EgyDeadProvider/src/main/kotlin/com/egydead/EgyDeadProvider.kt
@@ -0,0 +1,8 @@
+package com.egydead
+
+import com.lagradost.cloudstream3.*
+
+class EgyDeadProvider : MainAPI() {
+    override var name = "EgyDead"
+    override var mainUrl = "https://egydead.fyi"
+}
